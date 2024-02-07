5c1e0927b27823bc91011c3426184683e5137fd8
AndresML <moranandres79@gmail.com>
2024-02-07 03:52:16 +0100

    Añadiendo proyecto snake

---
+++ src/main/java/com/andresgmoran/objects/Apple.java
@@ -0,0 +1,23 @@
+package com.andresgmoran.objects;
+
+import com.andresgmoran.snakegame.SnakeGame;
+
+public class Apple {
+
+    SnakeGame main;
+
+    public int posX;
+    public int posY;
+
+    public Apple(int startX, int startY) {
+        posX = startX;
+        posY = startY;
+
+    }
+
+    @SuppressWarnings("static-access")
+    public void setRandomPosition() {
+        posX = (int) (Math.random() * main.WIDTH);
+        posY = (int) (Math.random() * main.HEIGHT);
+    }
+}
